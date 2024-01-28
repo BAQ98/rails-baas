f5149e8929100ee0873c18b7b7a964ddfe810a1c
baoqh98 <hoquocbaocyber@gmail.com>
2024-01-28 09:25:22 +0700

    fix: rollback migrate to add profile to column, create author references

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -2,5 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Card, type: :model do
   it { should validate_presence_of(:title) }
-  it { should belong_to :kanban_column }
+  it { should belong_to(:kanban_column) }
 end
