45a98692e59c2049e46bb4b9f04b6cea30577dfd
baoqh98 <hoquocbaocyber@gmail.com>
2024-02-18 15:11:04 +0700

    feat: add crud comment in card

---
+++ app/javascript/controllers/kanban/card_comment_controller.js
@@ -0,0 +1,19 @@
+import { Controller } from '@hotwired/stimulus';
+
+// Connects to data-controller="kanban--card-comment"
+export default class extends Controller {
+  static targets = ['comment'];
+
+  connect() {
+    
+  }
+
+  toggleComment(e) {
+    this.commentTargets.forEach(item => {
+      if (item.dataset.targetId === e.currentTarget.dataset.commentButtonId) {
+        item.querySelector('span').classList.toggle('hidden');
+        item.querySelector('form').classList.toggle('hidden');
+      }
+    });
+  }
+}
