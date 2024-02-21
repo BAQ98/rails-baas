f360cf1cf76114579495867552a56cce0bda0379
baoqh98 <hoquocbaocyber@gmail.com>
2024-02-21 10:29:22 +0700

    feat(deploy): render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,7 +3,8 @@
 set -o errexit
 
 bundle install
+bundle exec rails tailwindcss:install
+bundle exec rails tailwindcss:build
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
-bundle exec rails tailwindcss:build
