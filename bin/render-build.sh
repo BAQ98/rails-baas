c72c355f04fd6a4e958c73cdee451d23d1a177b3
baoqh98 <hoquocbaocyber@gmail.com>
2024-02-21 23:48:30 +0700

    feat(deploy): fix render-buid.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,7 +3,10 @@
 set -o errexit
 
 bundle install
+bundle lock --add-platform x86_64-linux
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails tailwindcss:build
 bundle exec rails tailwindcss:install
+./node_modules/.bin/tailwindcss -i ./app/assets/stylesheets/application.tailwind.css -o ./app/assets/builds/application.css
+bundle exec rails db:migrate
