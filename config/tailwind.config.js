595bdbcae7af1b582c21bec512cc963298cd84d0
baoqh98 <hoquocbaocyber@gmail.com>
2024-02-22 11:16:45 +0700

    feat(deploy): render-build.sh

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -21,5 +21,12 @@ module.exports = {
         }
       }
     }
-  }
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+    require('tailwindcss-animate')
+  ]
 };
