b62aac3646cbac1aecfeeb3afc185645cf009cd4
baoqh98 <hoquocbaocyber@gmail.com>
2024-02-22 10:25:23 +0700

    feat(deploy): yarn

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,12 +1,12 @@
 # Pin npm packages by running ./bin/importmap
 
 pin 'application', preload: true
-pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin '@kanety/stimulus-static-actions',
     to: 'https://ga.jspm.io/npm:@kanety/stimulus-static-actions@1.0.1/dist/index.modern.js', preload: true
+pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin 'stimulus-use', to: 'https://ga.jspm.io/npm:stimulus-use@0.51.3/dist/index.js', preload: true
 pin 'stimulus-dropdown', to: 'https://ga.jspm.io/npm:stimulus-dropdown@2.1.0/dist/stimulus-dropdown.mjs', preload: true
 pin 'hotkeys-js', to: 'https://ga.jspm.io/npm:hotkeys-js@3.10.4/dist/hotkeys.esm.js', preload: true
